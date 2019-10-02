65b07cac3b45295b0b6ff1b660c595649dd8a967
Chance Penner <chance.penner2016@ku.edu>
2019-10-02 10:22:36 -0500

    Add exercise 1

---
+++ exercise1/multiplicationtable.php
@@ -0,0 +1,37 @@
+<!-- helloWorld.php -->
+
+<?php
+   echo "<p>Multiplication Table!</p>";
+?>
+
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+//Inside myfirstprogram.php
+function multiply($x, $y) {
+    $z = $x * $y;
+    return $z;
+}
+
+
+$cols = 100;
+$rows = 100;
+
+ini_set("display_errors", 1);
+
+echo "<table border =\"1\">";
+
+for($i = 1; $i <= $rows; $i++)
+{
+  echo '<tr>';
+  for($j = 1; $j <=$cols; $j++)
+  {
+    echo '<td>' .multiply($i,$j). '</td>';
+  }
+    echo '</tr>';
+}
+echo "</table>";
+
+
+?>
