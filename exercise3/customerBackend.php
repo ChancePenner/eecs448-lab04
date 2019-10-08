9b6828cb222f37804450532e7d4f19dd44fa8b50
Chance Penner <chance.penner2016@ku.edu>
2019-10-08 16:49:12 -0500

    Implement basic recept

--- exercise3/customerBackend.php
+++ exercise3/customerBackend.php
@@ -1,1 +1,146 @@
  <!-- customerBackend.php -->
+ <?php
+ error_reporting(E_ALL);
+ ini_set("display_errors", 1);
+
+$password = $_POST["password"];
+
+$charizardAmount = $_COOKIE['charizardNumber'];
+$tyranitarAmount = $_COOKIE['tyranitarNumber'];
+$heracrossAmount = $_COOKIE['heracrossNumber'];
+
+$shippingType = $_POST["shipping"];
+$shippingAmount = 0;
+
+if($shippingType == "Free 7 day")
+{
+  $shippingAmount = 0;
+}
+else if($shippingType == "$50.00 Overnight")
+{
+  $shippingAmount = 50;
+}
+else if($shippingType == "$5.00 three day")
+{
+  $shippingAmount = 5;
+}
+
+echo "Thank you for your purchase!<br><br>";
+echo "Your password is: " . $password . "<br>";
+echo "Below is the receipt of your transaction:<br><br><br>";
+
+echo "<table border =\"1\">";
+
+echo "<tr>";
+
+
+echo "<td>";
+echo "";
+echo "</td>";
+
+echo "<td>";
+echo "Quantity";
+echo "</td>";
+
+echo "<td>";
+echo "Cost Per Item";
+echo "</td>";
+
+echo "<td>";
+echo "Sub total";
+echo "</td>";
+
+echo "</tr>";
+
+echo "<tr>";
+
+//////////////////////////////
+echo "<td>";
+echo "Charizard";
+echo "</td>";
+
+echo "<td>";
+echo $charizardAmount;
+echo "</td>";
+
+echo "<td>";
+echo "$100";
+echo "</td>";
+
+echo "<td>";
+echo "$" . "" . 100*$charizardAmount;
+echo "</td>";
+
+echo "</tr>";
+//////////////////////
+echo "<td>";
+echo "Tyranitar";
+echo "</td>";
+
+echo "<td>";
+echo $tyranitarAmount;
+echo "</td>";
+
+echo "<td>";
+echo "$20";
+echo "</td>";
+
+echo "<td>";
+echo "$" ."". 20*$tyranitarAmount;
+echo "</td>";
+
+echo "</tr>";
+//////////////////////////////////
+echo "<td>";
+echo "Heracross";
+echo "</td>";
+
+echo "<td>";
+echo $heracrossAmount;
+echo "</td>";
+
+echo "<td>";
+echo "$5";
+echo "</td>";
+
+echo "<td>";
+echo "$" . "" . 5*$heracrossAmount;
+echo "</td>";
+
+echo "</tr>";
+///////////////////////////
+
+echo "<td>";
+echo "Shipping";
+echo "</td>";
+//
+// echo "<td>";
+// echo $shipping;
+// echo "</td>";
+
+echo "<td colspan = '2'>";
+echo $shippingType;
+echo "</td>";
+
+
+
+// <td class="empty"> </td>
+//
+// echo "<td class="empty">"
+//
+//
+// "</td>";
+
+
+echo "<td>";
+echo "$" . "" . (5*$heracrossAmount + 100*$charizardAmount + 20*$tyranitarAmount + $shippingAmount);
+echo "</td>";
+
+echo "</tr>";
+
+
+
+echo "</table>";
+
+
+ ?>
--- exercise3/formChecker.js
+++ exercise3/formChecker.js
@@ -1,44 +1,71 @@
 // formChecker.js
+var amounts =
+{
+  charizardNumber: Number(document.querySelector("#charizardAmount")).value,
+  tyranitarNumber: Number(document.querySelector("#tyranitarAmount")).value,
+  heracrossNumber: Number(document.querySelector("#heracrossAmount")).value,
 
+  setAmounts: function()
+  {
+    this.charizardNumber= Number(document.querySelector("#charizardAmount").value);
+    this.tyranitarNumber= Number(document.querySelector("#tyranitarAmount").value);
+    this.heracrossNumber= Number(document.querySelector("#heracrossAmount").value);
+
+
+    document.cookie='charizardNumber=' + this.charizardNumber ;
+    document.cookie='tyranitarNumber=' + this.tyranitarNumber ;
+    document.cookie='heracrossNumber=' + this.heracrossNumber ;
+    //because I did not have my html "charizardAmount", "tyranitarAmount" and "heracrossAmount"
+    //in the php form, I had to do all this just to gain access to them in the php file
+
+  }
+};
+
+
+function setAmount()
+{
+  amounts.setAmounts();
+}
 
 function checkInput()
 {
-  alert("The form was submitted");
   if(checkAmountBought() && checkShipping() && checkUsername() && checkPassword())
   {
-    event.preventDefault()
-return false;
-    return true;
+    setAmount();
   }
   else
   {
+
     event.preventDefault()
     return false;
   }
-  event.preventDefault()
 
 }
 
 function checkAmountBought()
 {
-
+  setAmount();
   let charizardAmount = document.querySelector("#charizardAmount").value;
-  console.log(charizardAmount);
+  // console.log(charizardAmount);
 
   let tyranitarAmount = document.querySelector("#tyranitarAmount").value;
-  console.log(tyranitarAmount);
+  // console.log(tyranitarAmount);
 
   let heracrossAmount = document.querySelector("#heracrossAmount").value;
-  console.log(heracrossAmount);
+  // console.log(heracrossAmount);
 
-
+  if(charizardAmount.includes(" ") || tyranitarAmount.includes(" ") || heracrossAmount.includes(" "))
+  {
+    alert("Please only type numbers in the box. No spaces or letters allowed");
+    return false;
+  }
 
   //check that there is input in all boxes
   if(charizardAmount.length<1 || tyranitarAmount.length < 1 || heracrossAmount.length < 1)
   {
-    console.log("empty");
-    // return false;
-    return;
+    // console.log("empty");
+    alert("Please select how many you wish to purchase.")
+    return false;
   }
 
   charizardAmount = Number(charizardAmount);
@@ -47,34 +74,88 @@ function checkAmountBought()
 
   if(charizardAmount < 0 || tyranitarAmount < 0 || heracrossAmount < 0)
   {
-    // return false;
-    console.log("negative number");
-    return;
+    // console.log("negative number");
+    return false;
   }
   if(Number.isInteger(charizardAmount) && Number.isInteger(tyranitarAmount) && Number.isInteger(heracrossAmount))
   {
-    console.log("ALL INTS");
-    // return true;
+    // console.log("ALL INTS");
+    return true;
   }
   else
   {
-    console.log("not ints");
-    // return false;
+    // console.log("not ints");
+    alert("Please only type numbers in the box. No spaces or letters allowed");
+    return false;
   }
 
 }
 
 function checkShipping()
 {
-
+  let radioButtons = document.getElementsByTagName("input");
+  for(let i=0;i<radioButtons.length;i++)
+  {
+    if(radioButtons[i].type == "radio" && radioButtons[i].checked)
+    {
+      // alert("At least one is checked");
+      return true;
+    }
+  }
+  alert("Please select a shipping option");
+  return false;
 }
 
 function checkUsername()
 {
+    // console.log("checkUsername()");
 
+    username = document.querySelector("#username").value;
+    // console.log(username);
+
+   if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(username))
+   {
+     return true;
+   }
+   else
+   {
+     alert("Please enter a username in the form of an e-mail");
+     return false;
+   }
 }
 
 function checkPassword()
 {
+  let password = document.querySelector("#password").value;
+  if(password.length < 1)
+  {
+    alert("Please enter your password");
+    return false;
+  }
+  else
+  {
+    return true;
+  }
+}
 
+function clearInput()
+{
+  let inputs = document.getElementsByTagName("input");
+  for(let i=0;i<inputs.length;i++)
+  {
+    if(inputs[i].value != "Submit" && inputs[i].type != "radio") //to not clear "Submit" on the button
+    {
+      inputs[i].value = "";
+    }
+    if(inputs[i].type == "radio")
+    {
+      inputs[i].checked = false;
+    }
+  }
+
+}
+
+function getCharizardAmount()
+{
+  return document.querySelector("#charizardAmount").value;
 }
